9942883879cd7c93fd8614a48b75e0c5e6b252d8
anfasmohammed <163500801+anfasmohammed@users.noreply.github.com>
2024-09-12 15:51:21 +0530

    final commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <Layout>
     <div className='flex items-center flex-col gap-6 justify-center w-full h-[70vh] bg-no-repeat bg-center bg-cover bg-[url(https://images.pexels.com/photos/27774130/pexels-photo-27774130/free-photo-of-float.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1/)] '> 
-    <div className='flex flex-col w-full gap-3 justify-center items-center text-orange-950 bg-red-50 bg-opacity-40 pb-3'>
+    <div className='flex flex-col w-full gap-3 justify-center items-center text-red-200 bg-black bg-opacity-20 pb-3'>
     <h1 className='text-2xl lg:text-6xl fond-bold'>HELLO RIDERS </h1>
     <h2 className='text-base lg:text-3xl font-semibold'>Welcome to the world of boating,feast your eyes up on the vast sea.</h2>
     </div>
